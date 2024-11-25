604dbeabfecf73eb01884d6849d7a4f22a473f79
Alejandro Barrientos <112975406+beofalejandro@users.noreply.github.com>
2024-11-24 19:30:30 -0600

    3.9 Stable - Update icons on products

--- assets/js/get/get-products.js
+++ assets/js/get/get-products.js
@@ -124,12 +124,18 @@ document.addEventListener("DOMContentLoaded", async () => {
         <div class="card">
           <img src="${producto.imagenulr}" class="card-img-top" alt="${producto.nombre}">
           <div class="card-body">
-             <h5 class="card-title">${producto.nombre}</h5>
-            <p class="card-text">${producto.descripcion}</p>
-            <p class="card-text">$${producto.precio}</p>
-            <button class="btn btn-primary" onclick="agregarAlCarrito('${producto.id}')">Agregar al carrito</button>
-            <button class="btn btn-secondary" onclick="agregarAFavoritos('${producto.id}')">Agregar a favoritos</button>
-          </div>
+        <h5 class="card-title">${producto.nombre}</h5>
+        <p class="card-text">${producto.descripcion}</p>
+        <p class="card-text">$${producto.precio}</p>
+        <div style="display: flex;">
+        <button class="btn btn-primary" onclick="agregarAlCarrito('${producto.id}')">Agregar a carrito</button>
+        <button class="btn btn-secondary" style="margin-left: 10px;" onclick="agregarAFavoritos('${producto.id}')">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+  <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15"/>
+</svg>
+        </div>
+        </button>
+        </div>
         </div>
       `;
       container.appendChild(productoDiv);
@@ -148,10 +154,14 @@ document.addEventListener("DOMContentLoaded", async () => {
         <div class="card">
           <img src="${producto.imagenulr}" class="card-img-top" alt="${producto.nombre}">
           <div class="card-body">
-            <h5 class="card-title">${producto.nombre}</h5>
-            <p class="card-text">${producto.descripcion}</p>
-            <p class="card-text">$${producto.precio}</p>
-            <button class="btn btn-danger" onclick="quitarDeFavoritos('${producto.id}')">Quitar de favoritos</button>
+        <h5 class="card-title">${producto.nombre}</h5>
+        <p class="card-text">${producto.descripcion}</p>
+        <p class="card-text">$${producto.precio}</p>
+        <button class="btn btn-danger" onclick="quitarDeFavoritos('${producto.id}')">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
+          </svg>
+        </button>
           </div>
         </div>
       `;
